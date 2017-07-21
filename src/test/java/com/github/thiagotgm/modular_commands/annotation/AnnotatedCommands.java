b5f27402366ec361fd2ab89630d3a034882db7e2
ThiagoTGM <thiagotgmgaming@gmail.com>
2017-07-21 02:36:09 -0300

    Added tests for annotated subcommands.

--- src/test/java/com/github/thiagotgm/modular_commands/annotation/AnnotatedCommands.java
+++ src/test/java/com/github/thiagotgm/modular_commands/annotation/AnnotatedCommands.java
@@ -2,16 +2,61 @@ package com.github.thiagotgm.modular_commands.annotation;
 
 import com.github.thiagotgm.modular_commands.api.CommandContext;
 import com.github.thiagotgm.modular_commands.command.annotation.MainCommand;
+import com.github.thiagotgm.modular_commands.command.annotation.SubCommand;
+import com.github.thiagotgm.modular_commands.command.annotation.SuccessHandler;
 
 public class AnnotatedCommands {
 
     @MainCommand(
             name = "Annotated main command",
-            aliases = { "ano" }
+            aliases = { "ano" },
+            subCommands = { "Annotated sub command" },
+            successHandler = "handle"
             )
     public void reply( CommandContext context ) {
         
         context.getReplyBuilder().withContent( "Annotated!" ).build();
+        
+    }
+    
+    @SubCommand(
+            name = "Annotated sub command",
+            aliases = { "hana" },
+            onSuccessDelay = 5000,
+            successHandler = "handle"
+            )
+    public void sub( CommandContext context ) {
+        
+        context.getReplyBuilder().withContent( "*cries*" ).build();
+        
+    }
+    
+    @MainCommand(
+            name = "Unloadable main command",
+            aliases = { "ano" },
+            subCommands = { "null" }
+            )
+    public void fail( CommandContext context ) {
+        
+        context.getReplyBuilder().withContent( "Annotated!" ).build();
+        
+    }
+    
+    @SubCommand(
+            name = "Unloadable sub command",
+            aliases = { "ano" },
+            subCommands = { "null" }
+            )
+    public void fail2( CommandContext context ) {
+        
+        context.getReplyBuilder().withContent( "Annotated!" ).build();
+        
+    }
+    
+    @SuccessHandler( "handle" )
+    public void success( CommandContext context ) {
+        
+        context.getReplyBuilder().withContent( "Success!" ).build();
         
     }
 
